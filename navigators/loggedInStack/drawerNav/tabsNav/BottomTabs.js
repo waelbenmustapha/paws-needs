735f25295b34aaf1e8b25385951a86d1704a8afc
Saber Mohamed <sabermed66@gmail.com>
2022-11-03 20:31:04 +0100

    finished bottom navigation style

--- navigators/loggedInStack/drawerNav/tabsNav/BottomTabs.js
+++ navigators/loggedInStack/drawerNav/tabsNav/BottomTabs.js
@@ -1,28 +1,208 @@
 import React from "react";
+import { View, Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeStack from "./tabsStacks/HomeStack";
 import ServicesStack from "./tabsStacks/ServicesStack";
 import Orders from "../../../../screens/Orders";
 import Profile from "../../../../screens/Profile";
 import ShopStack from "./tabsStacks/ShopStack";
+import Colors from "../../../../utils/Colors";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="home-tab" component={HomeStack} />
-      <Tab.Screen name="orders-tab" component={Orders} />
-      <Tab.Screen name="services-tab" component={ServicesStack} />
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: Colors.PRIMARY,
+        headerShown: false,
+        tabBarStyle: {
+          borderTopWidth: 0,
+          elevation: 0,
+          height: 65,
+          paddingBottom: 8,
+        },
+      }}
+    >
       <Tab.Screen
+        name="home-tab"
+        component={HomeStack}
+        options={{
+          tabBarLabel: "Home",
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "500",
+          },
+          tabBarIcon: ({ focused, color, size }) => {
+            return focused ? (
+              <View
+                style={{
+                  width: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Image
+                  source={require("../../../../assets/home-fill.png")}
+                  style={{ width: 24, height: 24 }}
+                />
+              </View>
+            ) : (
+              <View
+                style={{
+                  width: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Image
+                  source={require("../../../../assets/home-outline.png")}
+                  style={{ width: 24, height: 24 }}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="orders-tab"
+        component={Orders}
+        options={{
+          tabBarLabel: "Orders",
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "500",
+          },
+          tabBarIcon: ({ focused, color, size }) => {
+            return focused ? (
+              <View
+                style={{
+                  width: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Image
+                  source={require("../../../../assets/cart-fill.png")}
+                  style={{ width: 24, height: 24 }}
+                />
+              </View>
+            ) : (
+              <View
+                style={{
+                  width: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Image
+                  source={require("../../../../assets/cart-outline.png")}
+                  style={{ width: 24, height: 24 }}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="services-tab"
+        component={ServicesStack}
+        options={{
+          tabBarLabel: "Services",
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "500",
+          },
+          tabBarIcon: ({ focused, color, size }) => {
+            return focused ? (
+              <View
+                style={{
+                  width: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Image
+                  source={require("../../../../assets/services-fill.png")}
+                  style={{ width: 24, height: 24 }}
+                />
+              </View>
+            ) : (
+              <View
+                style={{
+                  width: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Image
+                  source={require("../../../../assets/services-outline.png")}
+                  style={{ width: 24, height: 24 }}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="profile-tab"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "500",
+          },
+          tabBarIcon: ({ focused, color, size }) => {
+            return focused ? (
+              <View
+                style={{
+                  width: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Image
+                  source={require("../../../../assets/profile-fill.png")}
+                  style={{ width: 24, height: 24 }}
+                />
+              </View>
+            ) : (
+              <View
+                style={{
+                  width: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <Image
+                  source={require("../../../../assets/profile-outline.png")}
+                  style={{ width: 24, height: 24 }}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      {/* ************* start of tabs will not be shown ************** */}
+      <Tab.Screen
+        name="shops-tab"
+        component={ShopStack}
         options={{
           tabBarItemStyle: { display: "none" },
         }}
-        name="shops-tab"
-        component={ShopStack}
       />
-
-      <Tab.Screen name="profile-tab" component={Profile} />
+      {/* ************* end of tabs will not be shown ************** */}
     </Tab.Navigator>
   );
 };
