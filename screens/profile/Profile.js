5f0a08b3d8064f6dbd0895d8bf05443ba1f5f959
Saber Mohamed <sabermed66@gmail.com>
2022-11-18 20:22:14 +0100

    new custom bottom sheet

--- screens/profile/Profile.js
+++ screens/profile/Profile.js
@@ -1,38 +1,78 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useRef, useCallback } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  Button,
+  TouchableOpacity,
+  useWindowDimensions,
+  StatusBar,
+} from "react-native";
 import { useAuth } from "../../context/AuthProvider";
+import BottomSheet from "../../components/BottomSheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Profile = ({ navigation }) => {
   const auth = useAuth();
+  const { height } = useWindowDimensions();
+  const bottomSheetRef = useRef();
+  const openBottomSheet = useCallback(() => {
+    bottomSheetRef.current.expand();
+  }, []);
 
   return (
-    <View style={{ marginTop: 50 }}>
-      <Text>Profile</Text>
-      <TouchableOpacity
-        style={{ borderWidth: 1, padding: 5, width: "80%" }}
-        onPress={() => {
-          navigation.navigate("edit-profile");
+    <GestureHandlerRootView
+      style={{
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+        backgroundColor: "#fff",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 20,
+          paddingVertical: 40,
         }}
       >
-        <Text>Edit profile</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{ borderWidth: 1, padding: 5, width: "80%" }}
-        onPress={() => {
-          navigation.navigate("mypets-stack");
-        }}
-      >
-        <Text>My pets</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{ borderWidth: 1, padding: 5, width: "80%" }}
-        onPress={() => {
-          auth.logout();
-        }}
-      >
-        <Text>Logout</Text>
-      </TouchableOpacity>
-    </View>
+        <Text style={{ fontSize: 24, fontWeight: "700", marginBottom: 20 }}>
+          Profile
+        </Text>
+        <View style={{ marginBottom: 20 }}>
+          <Button title="Open Bottom Sheet" onPress={() => openBottomSheet()} />
+        </View>
+        <BottomSheet
+          ref={bottomSheetRef}
+          activeHeight={height * 0.5}
+          backgroundColor={"white"}
+          backDropColor={"black"}
+        />
+        <TouchableOpacity
+          style={{ borderWidth: 1, padding: 5, marginBottom: 20 }}
+          onPress={() => {
+            navigation.navigate("edit-profile");
+          }}
+        >
+          <Text>Edit profile</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ borderWidth: 1, padding: 5, marginBottom: 20 }}
+          onPress={() => {
+            navigation.navigate("mypets-stack");
+          }}
+        >
+          <Text>My pets</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ borderWidth: 1, padding: 5, marginBottom: 20 }}
+          onPress={() => {
+            auth.logout();
+          }}
+        >
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
