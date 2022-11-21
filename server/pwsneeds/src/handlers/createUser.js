01ffce93fbd5c523d64a3dfc3b67d4dc35a6286f
Waelbenmustapha <wbenmustapha@yahoo.fr>
2022-11-21 22:17:26 +0100

    aa

--- server/pwsneeds/src/handlers/createUser.js
+++ server/pwsneeds/src/handlers/createUser.js
@@ -1,14 +1,28 @@
-const mongoose=require("mongoose");
-const connectDatabase = require("../database/db")
+const connectDatabase = require("../database/db");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 module.exports.handler = async (event, context) => {
-    context.callbackWaitsForEmptyEventLoop = false;
+  context.callbackWaitsForEmptyEventLoop = false;
 
-    try {
-        await connectDatabase(); 
-    } catch (error) {
-        
-    }
-  
+  try {
+    await connectDatabase();
+    const { name, email, password } = JSON.parse(event.body);
+    let userObj = {
+      name,
+      email,
+      password: bcrypt.hashSync(password, 10),
+    };
+    userObj = await User.create(userObj);
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(userObj),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
 };
-
- 
